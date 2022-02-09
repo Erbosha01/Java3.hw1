9f25ca6a5c39b0c3d4f37bec0d5be87a4fe7ac8c
Erbosha01 <aibasheverbol@gmail.com>
2022-02-09 22:26:46 +0600

    do glide

--- app/src/main/java/com/geektech/java3hw1/MainActivity.java
+++ app/src/main/java/com/geektech/java3hw1/MainActivity.java
@@ -3,12 +3,25 @@ package com.geektech.java3hw1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView car;
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        car = findViewById(R.id.img_car);
+        url = "https://i.pinimg.com/474x/23/ab/a6/23aba60b66ef08174bb7455c4a8a2d2f.jpg";
+
+        Glide
+        .with(this)
+        .load(url)
+        .into(car);
     }
 }
