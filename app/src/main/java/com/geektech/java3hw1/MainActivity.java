7419505f9e4592c3e81f673c1168239ec9a7492b
Erbosha01 <aibasheverbol@gmail.com>
2022-02-11 01:40:39 +0600

    add Error

--- app/src/main/java/com/geektech/java3hw1/MainActivity.java
+++ app/src/main/java/com/geektech/java3hw1/MainActivity.java
@@ -3,6 +3,9 @@ package com.geektech.java3hw1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -10,16 +13,65 @@ import com.bumptech.glide.Glide;
 public class MainActivity extends AppCompatActivity {
 
     private ImageView car;
+    private EditText one, two;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        initListeners();
+    }
+
+    private void init() {
         car = findViewById(R.id.img_car);
+        one = findViewById(R.id.et_one);
+        two = findViewById(R.id.et_two);
 
         Glide
-        .with(this)
-        .load("https://i.pinimg.com/474x/23/ab/a6/23aba60b66ef08174bb7455c4a8a2d2f.jpg")
-        .into(car);
+                .with(this)
+                .load("https://i.pinimg.com/474x/23/ab/a6/23aba60b66ef08174bb7455c4a8a2d2f.jpg")
+                .into(car);
     }
+
+    private void initListeners() {
+        one.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (one.getText().toString().isEmpty()) {
+                    one.setError("is Empty");
+                }
+            }
+        });
+        two.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (two.getText().toString().isEmpty()) {
+                    two.setError("is Empty");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+
 }
