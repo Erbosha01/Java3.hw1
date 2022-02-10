04629a02e48f5278e88338a1df88085710af0e40
Erbosha01 <aibasheverbol@gmail.com>
2022-02-10 21:21:10 +0600

    little bit change

--- app/src/main/java/com/geektech/java3hw1/MainActivity.java
+++ app/src/main/java/com/geektech/java3hw1/MainActivity.java
@@ -10,18 +10,16 @@ import com.bumptech.glide.Glide;
 public class MainActivity extends AppCompatActivity {
 
     private ImageView car;
-    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         car = findViewById(R.id.img_car);
-        url = "https://i.pinimg.com/474x/23/ab/a6/23aba60b66ef08174bb7455c4a8a2d2f.jpg";
 
         Glide
         .with(this)
-        .load(url)
+        .load("https://i.pinimg.com/474x/23/ab/a6/23aba60b66ef08174bb7455c4a8a2d2f.jpg")
         .into(car);
     }
 }
